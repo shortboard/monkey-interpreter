cbbbe38b3e1474830fd01c86e197bf5e807c358c
Damien Simpson <damosimpson89@gmail.com>
2023-06-29 20:38:06 +1000

    remove comments

--- repl/repl.go
+++ repl/repl.go
@@ -21,13 +21,9 @@ func Start(in io.Reader, out io.Writer) {
 			return
 		}
 
-		// io.Writer.Write() takes a byte slice, so we need to convert the string
-		// to a byte slice
 		line := scanner.Text()
 		l := lexer.New(line)
 
-		// We're going to print out all the tokens the lexer gives us until we get to the end of the input
-		// (i.e. when the lexer returns token.EOF)
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Fprintf(out, "%+v\n", tok)
 		}
